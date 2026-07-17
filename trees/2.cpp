a50df1180cf5104dd7335b51fb0720ca0543f6aa
suyashpundir <suyashpundir1404@gmail.com>
2026-07-17 14:47:14 +0530

    added mmodre qustion of trees

---
+++ trees/2.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Node{
+	public:
+	int data;
+	Node* left;
+	Node* right;
+	Node(int val){
+		data=val;
+		left=NULL;
+		right=NULL;
+
+	}
+	
+
+	
+
+
+
+
+
+};
